9145a4bcaf46731e81ce53b421d8a89b822337b6
mariem <86363907+MarieMoalla@users.noreply.github.com>
2022-03-08 20:19:01 +0100

    Add mom dialogue

---
+++ School Morning Routine/Assets/Scripts/NextDialogue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextDialogue : MonoBehaviour
+{
+    public GameObject dialogue;
+    public GameObject currentDialogue;
+    public void onOpenDialogue()
+    {
+        if(dialogue != null)    dialogue.SetActive(true);
+        currentDialogue.SetActive(false);
+    }
+}
