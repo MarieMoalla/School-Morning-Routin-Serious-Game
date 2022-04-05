bab3a6cf9f0f6e3eb81307b26892b69f781c140b
mariem <86363907+MarieMoalla@users.noreply.github.com>
2022-04-05 23:12:15 +0100

    Update Scene1
    -add dialogues

---
+++ School Morning Routine/Assets/Scripts/CaleraSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaleraSwitch : MonoBehaviour
+{
+    public Camera MainCamera;
+    public Camera overheadCamera;
+
+    // Call this function to disable FPS camera,
+    // and enable overhead camera.
+    public void ShowOverheadView() {
+        MainCamera.enabled = false;
+        overheadCamera.enabled = true;
+    }
+    
+    // Call this function to enable FPS camera,
+    // and disable overhead camera.
+    public void ShowMainView() {
+        MainCamera.enabled = true;
+        overheadCamera.enabled = false;
+    }
+}
---
+++ School Morning Routine/Assets/Scripts/Triggerer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class Triggerer : MonoBehaviour
+{
+public GameObject obj;
+
+void Start ()
+{
+    Load();
+}
+
+private IEnumerator Load() 
+{
+        yield return new WaitForSeconds(3);
+        obj.SetActive(true);
+}
+
+}
