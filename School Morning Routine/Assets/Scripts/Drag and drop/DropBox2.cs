72233f6cba4daa4d9c457fe9cb8719601b565776
mariem <86363907+MarieMoalla@users.noreply.github.com>
2022-05-13 01:51:07 +0100

    Updates

---
+++ School Morning Routine/Assets/Scripts/Drag and drop/DropBox2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class DropBox2 : MonoBehaviour, IDropHandler
+{
+
+    //win lost variables
+    public GameObject win;
+    public GameObject conceil;
+    public AudioSource winSound;
+    public AudioSource loseSound;
+
+    public void OnDrop (PointerEventData eventData)
+    {
+        
+        Debug.Log("OnDrop");
+        if(this.tag == "win"){win.SetActive(true);Debug.Log("You won");winSound.Play(0);}
+        else {conceil.SetActive(true);Debug.Log("You lost");loseSound.Play(0);}
+        
+    }
+}
+
